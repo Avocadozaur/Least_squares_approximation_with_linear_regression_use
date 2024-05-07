dce71dd06756e2cddb930c4524fea9edb0ef08fc
wiktor <wiktor.kuchta@op.pl>
2024-05-07 22:48:07 +0200

    Least squares approximation with use of linear regression

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-
-import numpy
-import numpy as np
-from scipy import optimize
 import matplotlib.pyplot as plt
+import numpy as np
 
 # xw = [-1.0, 0.0, 1.0, 2.0]
 # yw = [4.0, -1.0, 0.0, 7.0]
@@ -25,58 +22,59 @@ yw = np.array([4, -1, 0, 7])
 n = 2
 
 
-def AproksymacjaSr2LR(xw, yw, n):
-    n+=1
+def Least_squares_approx_wLR(xw, yw, n):
+    n += 1
     if n < 2:
         return ("źle")
     if len(xw) != len(yw):
         return ("źle")
-    #zrobiłem wiaderka na piramidkę z liczb która zawsze się będzie układać
-    #chodzi o te same liczby na przekątnych żeby się do macierzy wpisywały
-    #od x^0 do x^n, komicznie jest gdzieniegdzie zaimplementowane ale to działająca wersja alfa
-    Buckets = []
-    for i in range(2 * n - 1):
-        Buckets.append([])
-    counter, i, j = 0, 0, 0
-    while i != (n * n):
-        Buckets[counter + j].append(i)
-        counter += 1
-        i += 1
-        if counter == n:
-            j += 1
-            counter = 0
-    # return Buckets
+    def Buckets():
+    #my idea for implementing a matrix with subsequent powers of x diagonally
+    #it comes from the linear regression formulas: y=a0+a1*x+a2*x^2...an*x^n
+        Buckets = []
+        for i in range(2 * n - 1):
+            Buckets.append([])
+        counter, i, j = 0, 0, 0
+        while i != (n * n):
+            Buckets[counter + j].append(i)
+            counter += 1
+            i += 1
+            if counter == n:
+                j += 1
+                counter = 0
+        return Buckets
+
     M = np.zeros((n, n))
 
-    def SumowanieX(lista, stopien):
+    def XSum(x_array, power):
         suma = 0
-        for i in range(len(lista)):
-            suma += math.pow(lista[i], stopien)
+        for i in range(len(x_array)):
+            suma += math.pow(x_array[i], power)
         return suma
 
-    def SumowanieY(listaX, listaY, stopien):
+    def YSum(x_array, y_array, power):
         suma = 0
-        for i in range(len(listaX)):
-            if stopien == 0:
-                suma += listaY[i]
+        for i in range(len(x_array)):
+            if power == 0:
+                suma += y_array[i]
             else:
-                suma += listaY[i] * math.pow(listaX[i], stopien)
+                suma += y_array[i] * math.pow(x_array[i], power)
         return suma
 
     counter = 0
-    for bucket in Buckets:
+    for bucket in Buckets():
         for num in bucket:
-                a = num % n
-                b = num // n
-                M[b][a] = SumowanieX(xw, counter)
+            a = num % n
+            b = num // n
+            M[b][a] = XSum(xw, counter)
         counter += 1
     counter = 0
     W = np.zeros((1, n))
     for i in range(n):
-        W[0][i] = SumowanieY(xw, yw, counter)
+        W[0][i] = YSum(xw, yw, counter)
         counter += 1
 
-    result = numpy.matmul(numpy.linalg.inv(M), numpy.transpose(W))
+    result = np.matmul(np.linalg.inv(M), np.transpose(W))
 
     x_values = np.linspace(min(xw), max(xw), 100)
     y_values = np.zeros_like(x_values)
@@ -93,4 +91,4 @@ def AproksymacjaSr2LR(xw, yw, n):
     return result
 
 
-print(AproksymacjaSr2LR(xw, yw, n))
+print(Least_squares_approx_wLR(xw, yw, n))
